decd05826f65c498f2e2f33efc9ab97a7389e4f7
Anastasiia <anastasia.alexeenko@gmail.com>
2023-03-27 16:42:17 +0200

    ✨ country page component (static)

--- src/components/CountryPage.jsx
+++ src/components/CountryPage.jsx
@@ -7,6 +7,52 @@ export default function CountryPage (props) {
                 <div className="btn-back">
                     &#129044; Back
                 </div>
+                <div className="country">
+                    <div className="country__flag">
+                        <img src="./src/assets/flag.png" alt="*Country name* flag" />
+                    </div>
+                    <div className="country__info">
+                        <h1 className="country__name">Belgium</h1>
+                        <div className="country__cols">
+                            <div className="country__col">
+                                <p className="country__data">
+                                    <span>Native Name:</span> België
+                                </p>
+                                <p className="country__data">
+                                    <span>Population:</span> 11,319,511
+                                </p>
+                                <p className="country__data">
+                                    <span>Region:</span> Europe
+                                </p>
+                                <p className="country__data">
+                                    <span>Sub Region:</span> Western Europe
+                                </p>
+                                <p className="country__data">
+                                    <span>Capital:</span> Brussels
+                                </p>
+                            </div>
+                            <div className="country__col">
+                                <p className="country__data">
+                                    <span>Top Level Domain:</span> .be
+                                </p>
+                                <p className="country__data">
+                                    <span>Currencies:</span> Euro
+                                </p>
+                                <p className="country__data">
+                                    <span>Languages:</span> Dutch, French, German
+                                </p>
+                            </div>
+                        </div>
+                        <div className="country__borders">
+                            <p className="country__data">
+                                <span>Border Countries:</span>
+                            </p>
+                            <div className="border-country">France</div>
+                            <div className="border-country">Germany</div>
+                            <div className="border-country">Netherlands</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
