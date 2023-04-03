076aa3f20d19fae93fabf8c919a09347b1eba8a5
Anastasiia <anastasia.alexeenko@gmail.com>
2023-04-03 19:14:01 +0200

    🔎 search function implemented

--- src/components/Catalogue.jsx
+++ src/components/Catalogue.jsx
@@ -3,7 +3,7 @@ import CountryCard from "./CountryCard";
 import arrow from "../assets/arrow.svg"
 import dArrow from "../assets/d-arrow.svg"
 
-export default function Catalogue(props) {  
+export default function Catalogue(props) {
 
     // func that toggles style of the select menu
     function toggleSelect() {
@@ -15,32 +15,62 @@ export default function Catalogue(props) {
     function handleSearch() {
         setSearchValue(event.target.value)
     }
-    // console.log(searchValue);
+
+    const [countriesToShow, setCountriesToShow] = React.useState(props.countryData)
+
+    React.useEffect(() => {
+        if (searchValue === "") {
+            setCountriesToShow(props.countryData)
+        } else {
+            fetch(`https://restcountries.com/v3.1/name/${searchValue}`)
+                .then(res => res.json())
+                .then(data => setCountriesToShow(data))
+        }
+    }, [searchValue])
+
+    let countryCards;
 
     // var containing all the country cards to be displayed in the catalogue
-    const countryCards = props.countryData.map(country => {
-        return (
-            <CountryCard 
-                key={country.cca3}
-                flag={country.flags.svg}
-                id={country.cca3}
-                name={country.name.common} 
-                population={country.population}
-                region={country.region}
-                capital={country.capital}
-            />
-        ) 
-    })
+    if (!countriesToShow.length) {
+        countryCards = props.countryData.map(country => {
+            return (
+                <CountryCard
+                    key={country.cca3}
+                    flag={country.flags.svg}
+                    id={country.cca3}
+                    name={country.name.common}
+                    population={country.population}
+                    region={country.region}
+                    capital={country.capital}
+                />
+            )
+        })
+    } else {
+        countryCards = countriesToShow.map(country => {
+            return (
+                <CountryCard
+                    key={country.cca3}
+                    flag={country.flags.svg}
+                    id={country.cca3}
+                    name={country.name.common}
+                    population={country.population}
+                    region={country.region}
+                    capital={country.capital}
+                />
+            )
+        })
+    }
+
 
     return (
         <div className="catalogue">
             <div className="container">
                 <div className="nav">
                     <div className="input-wrap">
-                        <input 
-                            type="text" 
-                            placeholder="Search for a country..." 
-                            className="country-search" 
+                        <input
+                            type="text"
+                            placeholder="Search for a country..."
+                            className="country-search"
                             onChange={handleSearch}
                             value={searchValue}
                         />
--- src/components/CountryPage.jsx
+++ src/components/CountryPage.jsx
@@ -41,18 +41,18 @@ export default function CountryPage() {
     } else {                                     // else push their cca3 and theit names in arrayOfBorderCountries[]
         countries.map(country => {
             if (borderElements.includes(country.cca3)) {
-                arrayOfBorderCountries.push([
-                    country.cca3,
-                    country.name.common
-                ])
+                arrayOfBorderCountries.push({
+                    code: country.cca3,
+                    name: country.name.common
+                })
             }
         })
-    
+
         // out of arrayOfBorderCountries[], render div el-s with cca3 & name
         borderElementsToShow = arrayOfBorderCountries.map(country => {
             return (
-                <Link to={`/${country[0]}`} key={country[0]}>
-                    <div key={country[0]} className="border-country">{country[1]}</div>
+                <Link to={`/${country.code}`} key={country.code}>
+                    <div key={country.code} className="border-country">{country.name}</div>
                 </Link>
             )
         })
@@ -66,50 +66,52 @@ export default function CountryPage() {
                         <span>&#129044;</span> Back
                     </div>
                 </Link>
-                <div className="country">
-                    <div className="country__flag">
-                        <img src={thisCountry.flag} alt={`Flag of ${thisCountry.name}`} />
-                    </div>
-                    <div className="country__info">
-                        <h1 className="country__name">{thisCountry.name}</h1>
-                        <div className="country__cols">
-                            <div className="country__col">
-                                <p className="country__data">
-                                    <span>Native Name:</span> {thisCountry.nativeName}
-                                </p>
-                                <p className="country__data">
-                                    <span>Population:</span> {thisCountry.population}
-                                </p>
-                                <p className="country__data">
-                                    <span>Region:</span> {thisCountry.region}
-                                </p>
-                                <p className="country__data">
-                                    <span>Sub Region:</span> {thisCountry.subregion}
-                                </p>
-                                <p className="country__data">
-                                    <span>Capital:</span> {thisCountry.capital}
-                                </p>
+                {thisCountry.length == 0 ? <h2>Loading...</h2> :
+                    <div className="country">
+                        <div className="country__flag">
+                            <img src={thisCountry.flag} alt={`Flag of ${thisCountry.name}`} />
+                        </div>
+                        <div className="country__info">
+                            <h1 className="country__name">{thisCountry.name}</h1>
+                            <div className="country__cols">
+                                <div className="country__col">
+                                    <p className="country__data">
+                                        <span>Native Name:</span> {thisCountry.nativeName}
+                                    </p>
+                                    <p className="country__data">
+                                        <span>Population:</span> {thisCountry.population}
+                                    </p>
+                                    <p className="country__data">
+                                        <span>Region:</span> {thisCountry.region}
+                                    </p>
+                                    <p className="country__data">
+                                        <span>Sub Region:</span> {thisCountry.subregion}
+                                    </p>
+                                    <p className="country__data">
+                                        <span>Capital:</span> {thisCountry.capital}
+                                    </p>
+                                </div>
+                                <div className="country__col">
+                                    <p className="country__data">
+                                        <span>Top Level Domain:</span> {thisCountry.tld}
+                                    </p>
+                                    <p className="country__data">
+                                        <span>Currencies:</span> {thisCountry.currencies}
+                                    </p>
+                                    <p className="country__data">
+                                        <span>Languages:</span> {thisCountry.languages}
+                                    </p>
+                                </div>
                             </div>
-                            <div className="country__col">
+                            <div className="country__borders">
                                 <p className="country__data">
-                                    <span>Top Level Domain:</span> {thisCountry.tld}
+                                    <span>Border Countries:</span>
                                 </p>
-                                <p className="country__data">
-                                    <span>Currencies:</span> {thisCountry.currencies}
-                                </p>
-                                <p className="country__data">
-                                    <span>Languages:</span> {thisCountry.languages}
-                                </p>
+                                {borderElementsToShow}
                             </div>
                         </div>
-                        <div className="country__borders">
-                            <p className="country__data">
-                                <span>Border Countries:</span>
-                            </p>
-                            {borderElementsToShow}
-                        </div>
                     </div>
-                </div>
+                }
             </div>
         </div>
     )
