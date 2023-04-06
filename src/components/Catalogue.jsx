9762c8825d6dc5b3fed3a548ec7ee3bb37da88c4
Anastasiia <anastasia.alexeenko@gmail.com>
2023-04-06 15:46:34 +0200

    🌍 filter by region feature implemented

--- src/components/Catalogue.jsx
+++ src/components/Catalogue.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import CountryCard from "./CountryCard";
 import arrow from "../assets/arrow.svg"
 import dArrow from "../assets/d-arrow.svg"
+import close from "../assets/close.svg"
+import dClose from "../assets/d-close.svg"
 
 export default function Catalogue(props) {
 
@@ -10,12 +12,15 @@ export default function Catalogue(props) {
         document.getElementById("select__options").classList.toggle("hidden")
     }
 
+    // state keeping your search request
     const [searchValue, setSearchValue] = React.useState("")
 
     function handleSearch() {
+        removeFilters()
         setSearchValue(event.target.value)
     }
 
+    // state keeping data of countries to show
     const [countriesToShow, setCountriesToShow] = React.useState(props.countryData)
 
     React.useEffect(() => {
@@ -28,38 +33,44 @@ export default function Catalogue(props) {
         }
     }, [searchValue])
 
-    let countryCards;
-
-    // var containing all the country cards to be displayed in the catalogue
-    if (!countriesToShow.length) {
-        countryCards = props.countryData.map(country => {
-            return (
-                <CountryCard
-                    key={country.cca3}
-                    flag={country.flags.svg}
-                    id={country.cca3}
-                    name={country.name.common}
-                    population={country.population}
-                    region={country.region}
-                    capital={country.capital}
-                />
-            )
-        })
-    } else {
-        countryCards = countriesToShow.map(country => {
-            return (
-                <CountryCard
-                    key={country.cca3}
-                    flag={country.flags.svg}
-                    id={country.cca3}
-                    name={country.name.common}
-                    population={country.population}
-                    region={country.region}
-                    capital={country.capital}
-                />
-            )
-        })
+    // function that handles filter by region
+    function filterRegion (region) {
+        document.querySelector("#select > p").innerText = region
+        document.getElementById('remove-filter').style.display = "block"
+        fetch(`https://restcountries.com/v3.1/region/${region}`)
+            .then(res => res.json())
+            .then(data => setCountriesToShow(data))
     }
+
+    function removeFilters () {
+        document.querySelector("#select > p").innerText = "Filter by Region"
+        document.getElementById('remove-filter').style.display = "none"
+        setCountriesToShow(props.countryData)
+    }
+
+    // var that will contain all the country cards to be displayed in the catalogue
+    let countryCards;
+    let dataToShow;
+    
+    if (!countriesToShow.length) {
+        dataToShow = props.countryData
+    } else {
+        dataToShow = countriesToShow
+    }
+
+    countryCards = dataToShow.map(country => {
+        return (
+            <CountryCard
+                key={country.cca3}
+                flag={country.flags.svg}
+                id={country.cca3}
+                name={country.name.common}
+                population={country.population}
+                region={country.region}
+                capital={country.capital}
+            />
+        )
+    })
 
 
     return (
@@ -75,15 +86,18 @@ export default function Catalogue(props) {
                             value={searchValue}
                         />
                     </div>
-                    <div className="select" onClick={toggleSelect} >
-                        Filter by Region <img className="select__arrow" src={props.darkMode ? dArrow : arrow} />
-                        <ul className="select__options hidden" id="select__options">
-                            <li className="select__option" value="Africa">Africa</li>
-                            <li className="select__option" value="America">America</li>
-                            <li className="select__option" value="Asia">Asia</li>
-                            <li className="select__option" value="Europe">Europe</li>
-                            <li className="select__option" value="Oceania">Oceania</li>
-                        </ul>
+                    <div className="select-wrapper">
+                        <div className="select" id="select" onClick={toggleSelect} >
+                            <p> Filter by Region</p> <img className="select__arrow" src={props.darkMode ? dArrow : arrow} />
+                            <ul className="select__options hidden" id="select__options">
+                                <li onClick={() => filterRegion("Africa")} className="select__option" value="Africa">Africa</li>
+                                <li onClick={() => filterRegion("America")} className="select__option" value="America">America</li>
+                                <li onClick={() => filterRegion("Asia")} className="select__option" value="Asia">Asia</li>
+                                <li onClick={() => filterRegion("Europe")} className="select__option" value="Europe">Europe</li>
+                                <li onClick={() => filterRegion("Oceania")} className="select__option" value="Oceania">Oceania</li>
+                            </ul>
+                        </div>
+                        <img id="remove-filter" src={props.darkMode ? dClose : close} alt="Remove filters" onClick={removeFilters} />
                     </div>
                 </div>
                 <div className="countrycards-wrapper">
