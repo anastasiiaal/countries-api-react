ec77ca30e8df7ecf4f86c43c73ec2072c99863f6
Anastasiia <anastasia.alexeenko@gmail.com>
2023-04-07 18:00:15 +0200

    🖼 dark mode svg replaced

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import darkEmpty from "../assets/dark-empty.svg"
-import darkFull from "../assets/dark-full.svg"
+import moon from "../assets/moon.svg"
+import sun from "../assets/sun.svg"
 
 export default function Header(props) {
     return (
@@ -9,7 +9,7 @@ export default function Header(props) {
             <div className="container">
                 <Link className="logo" to={"/"}>Where in the world?</Link>
                 <div className="darkmode-toggler" onClick={props.clickHandler}>
-                    <img src={props.darkMode ? darkFull : darkEmpty} alt="Dark mode image" />
+                    <img src={props.darkMode ? sun : moon} alt="Dark mode image" />
                     <p>{props.darkMode ? "Light" : "Dark"} Mode</p>
                 </div>
             </div>
